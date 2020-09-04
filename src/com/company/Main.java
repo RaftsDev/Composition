fd9271cd33b3be3c0a46bb74cdc80f46249047d9
Ralph F <RaftsDev@gmail.com>
2020-09-04 11:32:04 -0400

    Feat: conducted Main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// walls
+        Wall wall1 = new Wall("west");
+        Wall wall2 = new Wall("north");
+        Wall wall3 = new Wall("east");
+        Wall wall4 = new Wall("south");
+
+//        bed
+
+        Bed bed = new Bed("western",2,3,2,1);
+
+//        ceiling
+        Ceiling ceiling = new Ceiling(7,333);
+
+//        lamp
+        Lamp lamp = new Lamp("classic",false,3);
+
+//        Bedroom
+
+        Bedroom bedroom = new Bedroom("Regular", bed, wall1, wall2, wall3, wall4, lamp, ceiling);
+
+        bedroom.getLamp().turnOn();
+        bedroom.makeBed();
     }
 }
