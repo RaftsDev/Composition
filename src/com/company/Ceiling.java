f7fc545681dc2382a0da87cdfa5148366cb5ca8c
Ralph F <RaftsDev@gmail.com>
2020-09-04 11:04:06 -0400

    Feat: Ceiling class

---
+++ src/com/company/Ceiling.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Ceiling {
+    private int height;
+    private int paintedCollor;
+
+    public Ceiling(int height, int paintedCollor) {
+        this.height = height;
+        this.paintedCollor = paintedCollor;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getPaintedCollor() {
+        return paintedCollor;
+    }
+}
